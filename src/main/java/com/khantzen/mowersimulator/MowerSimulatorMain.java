1cfc6ea4facd05f15744e5720492200dbc7e34ee
Kevin <kevin.hantzen@gmail.com>
2018-11-25 22:52:15 +0100

    Implement main class

---
+++ src/main/java/com/khantzen/mowersimulator/MowerSimulatorMain.java
@@ -0,0 +1,35 @@
+package com.khantzen.mowersimulator;
+
+import com.khantzen.mowersimulator.model.SimulatorEntry;
+import com.khantzen.mowersimulator.operation.Simulation;
+import com.khantzen.mowersimulator.parser.EntryFile;
+
+import java.util.List;
+
+public class MowerSimulatorMain {
+
+    public static void main(String[] args) throws Exception {
+        SimulatorEntry simulatorEntry = getSimulatorEntry(args);
+        List<String> simulationResult = runMowerSimulation(simulatorEntry);
+        printSimulationResult(simulationResult);
+    }
+
+    private static void printSimulationResult(List<String> simulationResult) {
+        simulationResult.forEach(System.out::println);
+    }
+
+    private static List<String> runMowerSimulation(SimulatorEntry simulatorEntry) {
+        Simulation simulation = new Simulation(simulatorEntry);
+        return simulation.run();
+    }
+
+    private static SimulatorEntry getSimulatorEntry(String[] args) throws Exception {
+        if (args.length == 0) {
+            throw new Exception("Program should take at least one argument in entry");
+        }
+
+        String filePath = args[0];
+        EntryFile entryFile = new EntryFile();
+        return entryFile.parse(filePath);
+    }
+}
